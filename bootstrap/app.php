49e079c630499e0544e7888a0e6aa645c9ed1ab5
jaysonbutawan <159876164+jaysonbutawan@users.noreply.github.com>
2026-02-13 16:30:04 +0800

    Initial commit

---
+++ bootstrap/app.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+
+    $middleware->append(\App\Http\Middleware\AuthMiddleware::class);
+
+        $middleware->group('api', [
+            EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
+    })
+    ->withExceptions(function ($exceptions) {
+    })->create();
